3be726ffcaf6092ec6ce6af9e45129a346fdcaf9
istarlet <istarlet@gmail.com>
2022-06-20 23:59:12 -0400

    Change javascript to async await

--- main.js
+++ main.js
@@ -3,17 +3,30 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.textContent = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.textContent = data.joke;
 }
 
-jokeBtn.addEventListener("click", generateJoke)
+// function generateJoke() {
+//     const config = {
+//       headers: {
+//         Accept: "application/json",
+//       },
+//     };
+//     fetch("https://icanhazdadjoke.com", config)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         jokeEl.textContent = data.joke;
+//       });
+//   }
+
+jokeBtn.addEventListener("click", generateJoke);
